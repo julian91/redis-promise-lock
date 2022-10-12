54cbf135dbf5a58c905a89e56acac2c6ea238e2e
Don Julio <julian.kaminski@bauerxcel.de>
2022-10-12 14:12:04 +0200

    feat: added linting to the project

--- src/lock.ts
+++ src/lock.ts
@@ -2,7 +2,7 @@ import { createClient } from 'redis'
 
 const packageName = 'redis-promise-lock'
 
-export type LockOptions = {
+export interface LockOptions {
   retryLimit: number
   retryDelay: number
   ttl: number
@@ -12,31 +12,30 @@ export type OptionalLockOptions = Partial<LockOptions>
 
 type RedisClient = ReturnType<typeof createClient>
 
-const sleep = (ms: number): Promise<void> => new Promise((resolve) => setTimeout(() => resolve(), ms))
+const sleep = async (ms: number): Promise<void> => await new Promise((resolve) => setTimeout(() => resolve(), ms))
 
 /**
  * @module Lock Class
  */
 export class Lock {
-
-  private client: RedisClient
-  private defaultRetryLimit = 10
-  private defaultRetryDelay = 100
-  private defaultTtl = 3
+  private readonly client: RedisClient
+  private readonly defaultRetryLimit: number = 10
+  private readonly defaultRetryDelay: number = 100
+  private readonly defaultTtl: number = 3
 
   /**
    * Lock Instance
-   * 
+   *
    * Default options object:
    * - options.retryLimit   number of times lock is trying to be set before bailing out (default: 10)
    * - options.retryDelay   amount of time in ms to wait between retries (default: 100)
    * - options.ttl          ttl of the lock to be set in redis before its removed by redis itself in seconds (default: 3)
-   * 
+   *
    * @param {RedisClient} redisClient     a promise based redis client (default in v4.x)
    * @param {Object} options              [default values for lock instance]
    */
-  constructor(redisClient: RedisClient, options = {} as OptionalLockOptions) {
-    if (!redisClient) throw new Error(`[${packageName}] - No redisClient provided.`)
+  constructor (redisClient: RedisClient, options: OptionalLockOptions = {}) {
+    if (redisClient === undefined) throw new Error(`[${packageName}] - No redisClient provided.`)
     this.client = redisClient
     const defaultOptions = this.getOptions(options)
     this.defaultRetryLimit = defaultOptions.retryLimit
@@ -49,8 +48,8 @@ export class Lock {
    * @param lockName part of redis key after the prefix
    * @returns the prefix:lockName as combined string
    */
-  public getRedisKey(lockName: string): string {
-    if (!lockName || typeof lockName !== 'string') throw new Error(`[${packageName}] - Invalid lockName provided! Must be a non empty string.`)
+  public getRedisKey (lockName: string): string {
+    if (typeof lockName !== 'string' || lockName.length === 0) throw new Error(`[${packageName}] - Invalid lockName provided! Must be a non empty string.`)
     return `${packageName}:${lockName}`
   }
 
@@ -59,7 +58,7 @@ export class Lock {
    * @param lockSpecificOptions optional overrides of default lock options
    * @returns merged LockOptions, falls back to default if no OptionalLockOptions provided
    */
-  public getOptions(lockSpecificOptions = {} as OptionalLockOptions): LockOptions {
+  public getOptions (lockSpecificOptions: OptionalLockOptions = {}): LockOptions {
     const options = { // use default if no lock-specific stuff
       retryLimit: lockSpecificOptions.retryLimit !== undefined ? lockSpecificOptions.retryLimit : this.defaultRetryLimit,
       retryDelay: lockSpecificOptions.retryDelay !== undefined ? lockSpecificOptions.retryDelay : this.defaultRetryDelay,
@@ -80,10 +79,10 @@ export class Lock {
    * @param ttl maximum amount of time the lock lives in redis - infinite if 0
    * @returns boolean whether lock has been applied or not
    */
-  public async applyLock(key: string, payload: string, ttl: number): Promise<boolean> {
+  public async applyLock (key: string, payload: string, ttl: number): Promise<boolean> {
     const lockApplied = await this.client.setNX(key, payload) as unknown as boolean
     // We check for lockApplied so existing Lock ttl does not get extended - see acquireLock loop
-    if (ttl && lockApplied) {
+    if (ttl !== 0 && lockApplied) {
       await this.client.expire(key, ttl)
     }
     return lockApplied
@@ -93,23 +92,23 @@ export class Lock {
    * Deletes the lock from redis
    * @param lockName part of redis key after the prefix
    */
-  public async releaseLock(lockName: string): Promise<void> {
+  public async releaseLock (lockName: string): Promise<void> {
     const key = this.getRedisKey(lockName)
     await this.client.del(key)
   }
 
   /**
    * Tries to acquire a lock
-   * 
+   *
    * - If a lock already exists, tries again X (lockSpecificOptions.retryLimit) times
    * - Waits Y ms (lockSpecificOptions.retryDelay) between tries
    * - Sets a ttl (lockSpecificOptions.ttl) on the lock if it could be acquired
-   * 
+   *
    * @param lockName part of redis key after the prefix
    * @param lockSpecificOptions optional overrides of default lock options
    * @returns boolean whether lock has been applied or not
    */
-  public async acquireLock(lockName: string, lockSpecificOptions = {} as OptionalLockOptions): Promise<boolean> {
+  public async acquireLock (lockName: string, lockSpecificOptions: OptionalLockOptions = {}): Promise<boolean> {
     const { retryLimit, retryDelay, ttl } = this.getOptions(lockSpecificOptions)
 
     const key = this.getRedisKey(lockName)
--- tests/lock.test.ts
+++ tests/lock.test.ts
@@ -1,58 +1,45 @@
-// @ts-nocheck
+
+import { createClient } from 'redis'
 import { Lock } from '../src/lock'
 
-const defaultMockedClient = {
-  setNX: async () => { },
-  expire: async () => { },
-  del: async () => { }
-}
+type RedisClient = ReturnType<typeof createClient>
 
-const packageName = 'redis-promise-lock'
+const defaultMockedClient = {
+  setNX: async () => {},
+  expire: async () => {},
+  del: async () => {}
+} as unknown as RedisClient
 
 const defaultRetryLimit = 10
-const defaultRetryDelay = 1000
+const defaultRetryDelay = 100
 const defaultTtl = 3
 
 describe('test initialization of class', () => {
-
   it('should crash without valid redis instance', () => {
-    expect(() => { new Lock(undefined) }).toThrowError(`[${packageName}] - No redisClient provided.`)
+    // @ts-expect-error - we explicitly want to check for our friends that dont use typescript whether they pass wrong args to a func
+    expect(() => { const lock = new Lock(undefined) }).toThrowError(/No redisClient provided/) // eslint-disable-line @typescript-eslint/no-unused-vars
   })
 
   it('should create redis instance', () => {
-    expect(() => { new Lock(defaultMockedClient) }).not.toThrowError()
+    expect(() => { const lock = new Lock(defaultMockedClient) }).not.toThrowError() // eslint-disable-line @typescript-eslint/no-unused-vars
   })
 
   it('should override default options if provided', () => {
-
     const l1 = new Lock(defaultMockedClient)
-    expect(l1.getOptions()).toEqual({
-      retryLimit: defaultRetryLimit,
-      retryDelay: defaultRetryDelay,
-      ttl: defaultTtl
-    })
-
-    const overriddenOptions = {
-      retryLimit: 69,
-      retryDelay: 420,
-      ttl: 1337
-    }
+    expect(l1.getOptions()).toEqual({ retryLimit: defaultRetryLimit, retryDelay: defaultRetryDelay, ttl: defaultTtl })
+    const overriddenOptions = { retryLimit: 69, retryDelay: 420, ttl: 1337 }
     const l2 = new Lock(defaultMockedClient, overriddenOptions)
     expect(l2.getOptions()).toEqual(overriddenOptions)
-
   })
-
 })
 
 describe('test getOptions', () => {
-
   it('should throw on invalid paramater types', () => {
     const l1 = new Lock(defaultMockedClient)
-    expect(() => { l1.getOptions({ retryLimit: 'NaN' }) }).toThrowError(`[${packageName}] - Invalid retryLimit provided! Must be a number greater than 0.`)
-
-    expect(() => { l1.getOptions({ retryDelay: 0 }) }).toThrowError(`[${packageName}] - Invalid retryDelay provided! Must be a number greater than 0.`)
-
-    expect(() => { l1.getOptions({ retryLimit: -1 }) }).toThrowError(`[${packageName}] - Invalid retryLimit provided! Must be a number greater than 0.`)
+    // @ts-expect-error - we explicitly want to check for our friends that dont use typescript whether they pass wrong args to a func
+    expect(() => { l1.getOptions({ retryLimit: 'NaN' }) }).toThrowError(/Invalid retryLimit/)
+    expect(() => { l1.getOptions({ retryDelay: 0 }) }).toThrowError(/Invalid retryDelay/)
+    expect(() => { l1.getOptions({ retryLimit: -1 }) }).toThrowError(/Invalid retryLimit/)
   })
 
   it('should accept 0 as ttl value', () => {
@@ -62,100 +49,69 @@ describe('test getOptions', () => {
 
   it('should return options object with overridden parameters (rest default)', () => {
     const l1 = new Lock(defaultMockedClient)
-    const testObj6 = l1.getOptions({ retryLimit: 1337 })
-    const testObj9 = l1.getOptions({ retryDelay: 6969 })
-    const testObj420 = l1.getOptions({ ttl: 420 })
-
-    expect(testObj6).toMatchObject({
-      retryLimit: 1337,
-      retryDelay: defaultRetryDelay,
-      ttl: defaultTtl
-    })
-
-    expect(testObj9).toMatchObject({
-      retryLimit: defaultRetryLimit,
-      retryDelay: 6969,
-      ttl: defaultTtl
-    })
-
-    expect(testObj420).toMatchObject({
-      retryLimit: defaultRetryLimit,
-      retryDelay: defaultRetryDelay,
-      ttl: 420
-    })
+    const testObj1 = l1.getOptions({ retryLimit: 1337 })
+    const testObj2 = l1.getOptions({ retryDelay: 6969 })
+    const testObj3 = l1.getOptions({ ttl: 420 })
+    expect(testObj1).toMatchObject({ retryLimit: 1337, retryDelay: defaultRetryDelay, ttl: defaultTtl })
+    expect(testObj2).toMatchObject({ retryLimit: defaultRetryLimit, retryDelay: 6969, ttl: defaultTtl })
+    expect(testObj3).toMatchObject({ retryLimit: defaultRetryLimit, retryDelay: defaultRetryDelay, ttl: 420 })
   })
 })
 
 describe('test getRedisKey', () => {
-
   it('should throw on invalid paramater types', () => {
     const l1 = new Lock(defaultMockedClient)
-
-    expect(() => { l1.getRedisKey() }).toThrowError(`[${packageName}] - Invalid lockName provided! Must be a non empty string.`)
-
-    expect(() => { l1.getRedisKey(6969) }).toThrowError(`[${packageName}] - Invalid lockName provided! Must be a non empty string.`)
-
-    expect(() => { l1.getRedisKey('') }).toThrowError(`[${packageName}] - Invalid lockName provided! Must be a non empty string.`)
+    // @ts-expect-error - we explicitly want to check for our friends that dont use typescript whether they pass wrong args to a func
+    expect(() => { l1.getRedisKey() }).toThrowError(/Invalid lockName/)
+    // @ts-expect-error - we explicitly want to check for our friends that dont use typescript whether they pass wrong args to a func
+    expect(() => { l1.getRedisKey(6969) }).toThrowError(/Invalid lockName/)
+    expect(() => { l1.getRedisKey('') }).toThrowError(/Invalid lockName/)
   })
 })
 
 describe('test applyLock', () => {
-
   it('should set ttl to lock if ttl is greater than 0 and lock is new', async () => {
-
     const client = {
       setNX: jest.fn(async () => true),
-      expire: jest.fn(async (key, payload) => { })
-    }
+      expire: jest.fn(async (key, payload) => {})
+    } as unknown as RedisClient
     const l1 = new Lock(client)
-
     const lockApplied = await l1.applyLock('beer', 'tasty', 69)
     expect(lockApplied).toEqual(true)
     expect(client.expire).toHaveBeenCalled()
     expect(client.expire).toHaveBeenCalledWith('beer', 69)
-
   })
 
   it('should not add ttl if ttl is 0 (infinite ttl)', async () => {
-
     const client = {
       setNX: jest.fn(async () => true),
       expire: jest.fn(async (key, payload) => { })
-    }
+    } as unknown as RedisClient
     const l1 = new Lock(client)
-
     const lockApplied = await l1.applyLock('beer', 'tasty', 0)
     expect(lockApplied).toEqual(true)
     expect(client.expire).not.toHaveBeenCalled()
-
   })
 
   it('should not add ttl to existing lock', async () => {
-
     const client = {
       setNX: jest.fn(async () => false),
       expire: jest.fn(async (key, payload) => { })
-    }
+    } as unknown as RedisClient
     const l1 = new Lock(client)
-
     const lockApplied = await l1.applyLock('beer', 'tasty', 1337)
     expect(lockApplied).toEqual(false)
     expect(client.expire).not.toHaveBeenCalled()
-
   })
-
 })
 
 describe('test releaseLock', () => {
-
   it('should delete redisKey with prefix', async () => {
-
     const client = {
-      del: jest.fn(async (key) => { }),
-    }
+      del: jest.fn(async (key) => { })
+    } as unknown as RedisClient
     const l1 = new Lock(client)
     const expectedKey = l1.getRedisKey('gunna')
-
     await l1.releaseLock('gunna')
     expect(client.del).toHaveBeenCalled()
     expect(client.del).toHaveBeenCalledWith(expectedKey)
